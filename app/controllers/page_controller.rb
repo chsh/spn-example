9a758af5e87b1a659fd155814f684beada74a041
CHIKURA Shinsaku <shinsaku@chikura.me>
2013-11-24 10:32:13 +0900

    Fix indent.

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,6 +1,6 @@
 class PageController < ApplicationController
-	# User logged in context.
-	def start
-		# pass user#spn_token to view.
-	end
+  # User logged in context.
+  def start
+    # pass user#spn_token to view.
+  end
 end
--- app/models/spn.rb
+++ app/models/spn.rb
@@ -1,30 +1,31 @@
 class Spn < ActiveRecord::Base
-	belongs_to :user
-	attr_accessible :device_token
+  belongs_to :user
+  attr_accessible :device_token
 
-	scope :device_token, -> token { where(device_token: token) }
-	scope :by_user, -> user_id { where(user_id: user_id) }
+  scope :device_token, -> token { where(device_token: token) }
+  scope :by_user, -> user_id { where(user_id: user_id) }
 
-	def self.pusher
-		@@pusher = build_pusher
-	end
+  def self.pusher
+    @@pusher = build_pusher
+  end
 
-	# params: { title: "", body: "", path: '/hello'} + optional {action: 'Read'}
-	def push(params)
-		n = build_notification_from_params params
-		self.class.pusher.push n
-	end
+  # params: { title: "", body: "", path: '/hello'} + optional {action: 'Read'}
+  def push(params)
+    n = build_notification_from_params params
+    self.class.pusher.push n
+  end
 
-	private
-	def build_notification_from_params(params)
-		params = params.merge device_token: self.device_token
-		Grocer::SafariNotification.new(params)
-	end
-	def self.build_pusher
-		Grocer.pusher(
-				certificate: ENV['PEM_PATH'],
-				passphrase: ENV['PEM_PASS_PHRASE'],
-				gateway: 'gateway.push.apple.com',
-		)
-	end
+  private
+  def build_notification_from_params(params)
+    params = params.merge device_token: self.device_token
+    Grocer::SafariNotification.new(params)
+  end
+
+  def self.build_pusher
+    Grocer.pusher(
+        certificate: ENV['PEM_PATH'],
+        passphrase: ENV['PEM_PASS_PHRASE'],
+        gateway: 'gateway.push.apple.com',
+    )
+  end
 end
