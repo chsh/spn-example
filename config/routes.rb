b55364809984c796b77e12ae249214bde5ffe3a6
CHIKURA Shinsaku <shinsaku@chikura.me>
2013-11-24 10:37:02 +0900

    Reformat code.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Your::Application.routes.draw do
 
-	scope '/spn/push/v1', controller: 'spn' do
-		post 'pushPackages/YOUR-WEBSITE-PUSH-ID', action: 'download'
-		post 'log', action: 'log'
-		post 'devices/:device_token/registrations/YOUR-WEBSITE-PUSH-ID',
-				 action: 'create'
-		delete 'devices/:device_token/registrations/YOUR-WEBSITE-PUSH-ID',
-				 action: 'destroy'
-	end
+  scope '/spn/push/v1', controller: 'spn' do
+    post 'pushPackages/YOUR-WEBSITE-PUSH-ID', action: 'download'
+    post 'log', action: 'log'
+    post 'devices/:device_token/registrations/YOUR-WEBSITE-PUSH-ID',
+         action: 'create'
+    delete 'devices/:device_token/registrations/YOUR-WEBSITE-PUSH-ID',
+           action: 'destroy'
+  end
 
   get '/start', 'page#start'
 
